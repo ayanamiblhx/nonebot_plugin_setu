517055391be519d03771e9f8c347c30c74216a8c
ayanamiblhx <2927652162@qq.com>
2023-03-23 21:22:45 +0800

    :zap: 魔法没有设置时默认关闭魔法

--- nonebot_plugin_setu/proxies.py
+++ nonebot_plugin_setu/proxies.py
@@ -1,21 +1,24 @@
 from httpx_socks import AsyncProxyTransport
 from nonebot.log import logger
 
-from .create_file import Config
+from .file_tools import Config
 
 proxy_config = Config()
 
-socks = proxy_config.proxies_socks
-http = proxy_config.proxies_http
+socks = proxy_config.get_file_args('PROXIES_SOCKS')
+http = proxy_config.get_file_args('PROXIES_HTTP')
 if socks != '':
-    logger.info('已配置socks代理')
+    logger.info('已配置socks代理,http代理将被忽略')
+    proxy_config.set_file_args('PROXIES_SWITCH', 1)
     proxy_socks = AsyncProxyTransport.from_url(socks)
     proxy_http = None
 elif http != '':
-    logger.info('已配置http代理')
+    logger.info('已配置http代理,socks代理将被忽略')
+    proxy_config.set_file_args('PROXIES_SWITCH', 1)
     proxy_socks = None
     proxy_http = http
 else:
     logger.info('未配置代理')
+    proxy_config.set_file_args('PROXIES_SWITCH', 0)
     proxy_socks = None
     proxy_http = None
