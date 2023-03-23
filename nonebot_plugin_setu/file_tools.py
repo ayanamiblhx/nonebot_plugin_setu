fa5b451e2dc2cd5b1e8dac5d5f6205f9d90cc206
ayanamiblhx <2927652162@qq.com>
2023-03-23 21:24:52 +0800

    :sparkles: 新增修改文件参数方法

--- nonebot_plugin_setu/file_tools.py
+++ nonebot_plugin_setu/file_tools.py
@@ -7,15 +7,15 @@ setu_config = {
     'SUPERUSERS': [""],
     'PROXIES_HTTP': '',
     'PROXIES_SOCKS': '',
-    'PROXIES_SWITCH': 1,
-    'ONLINE_SWITCH': 0,
+    'PROXIES_SWITCH': 0,
+    'ONLINE_SWITCH': 1,
 }
 
 
 class Config:
     def __init__(self):
         self.create_file()
-        with open('data/setu_config.json', 'r') as file:
+        with open('data/setu_config.json', 'r', encoding='utf-8') as file:
             self.config = json.load(file)
         self.super_users = self.config['SUPERUSERS']
         self.proxies_http = self.config['PROXIES_HTTP']
@@ -29,16 +29,25 @@ class Config:
         pathlib.Path('loliconImages/r18').mkdir(parents=True, exist_ok=True)
         if not os.path.exists('data/setu_config.json'):
             with open('data/setu_config.json', 'w', encoding='utf-8') as f:
-                json.dump(setu_config, f, ensure_ascii=False, indent=4)
+                json.dump(setu_config, f, indent=4)
         if not os.path.exists('data/lolicon.db'):
             conn = sqlite3.connect('data/lolicon.db')
             conn.close()
 
-    def get_file_args(self, args_name: str):
+    @staticmethod
+    def get_file_args(args_name: str):
         with open('data/setu_config.json', 'r', encoding='utf-8') as file:
             content = json.load(file)
-            if not args_name in content:
+            if args_name not in content:
                 content.update({args_name: ' '})
                 with open('data/setu_config.json', 'w', encoding='utf-8') as file_new:
                     json.dump(content, file_new, indent=4)
             return content[args_name]
+
+    @staticmethod
+    def set_file_args(args_name: str, args_value: str):
+        with open('data/setu_config.json', 'r', encoding='utf-8') as file:
+            setu_content = json.load(file)
+            setu_content.update({args_name: args_value})
+            with open('data/setu_config.json', 'w', encoding='utf-8') as file_new:
+                json.dump(setu_content, file_new, indent=4)
